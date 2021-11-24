38b079f7217a13773219ea0e3e30a0975e3e085b
sussydevil <sussydevil@gmail.com>
2021-11-24 18:54:15 +0300

    Initial Commit

---
+++ ORK Checker/ORK_CheckerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ORK_CheckerApp.swift
+//  ORK Checker
+//
+//  Created by Ilya Komarov on 24.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct ORK_CheckerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
